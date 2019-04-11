bda2e74054afabc816b26464b5a35a8d9a9bdb2b
Chris Cedeno <chrisrey95@gmail.com>
2019-04-11 17:20:12 -0400

    parse url

--- src/client.c
+++ src/client.c
@@ -44,10 +44,19 @@ urlinfo_t *parse_url(char *url)
     5. Set the port pointer to 1 character after the spot returned by strchr.
     6. Overwrite the colon with a '\0' so that we are just left with the hostname.
   */
-
   ///////////////////
   // IMPLEMENT ME! //
   ///////////////////
+
+  path = strchr(url, "/");
+  urlinfo->path = path + 1;
+  path = '\0';
+
+  port = strstr(url, ":");
+  urlinfo->port = port + 1;
+  port = '\0';
+
+  urlinfo->hostname = url;
 
   return urlinfo;
 }
